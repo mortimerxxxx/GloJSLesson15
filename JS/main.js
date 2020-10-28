d75f8287e4e86f5049b45169fb19c25acdb05555
Ilia Shevaldin <blacktor666@gmail.com>
2020-10-28 23:15:24 +0300

    Домашка номнер 15 частьь 2

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class First {
+
+    hello(){
+        console.log('Привет я метод родителя!');
+    }
+
+}
+
+class Second extends First {
+    hello(){
+        super.hello();
+        console.log('А я наследуемый метод');
+    }
+}
+
+let strat = new Second();
+
+strat.hello();
